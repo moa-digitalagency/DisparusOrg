c035de4bc91c27fa645638c285fe353b356ba87e
My One Art - Digital Agency <93324793+moa-digitalagency@users.noreply.github.com>
2026-01-22 17:43:06 +0100

    Create sitemap_generator.py for dynamic sitemaps

---
+++ utils/sitemap_generator.py
@@ -0,0 +1,23 @@
+class SitemapGenerator:
+    def __init__(self, base_url):
+        self.base_url = base_url
+        self.sitemap_urls = []
+
+    def add_url(self, url, lastmod=None, changefreq='monthly', priority=None):
+        url_entry = {'loc': url}
+        if lastmod:
+            url_entry['lastmod'] = lastmod
+        if changefreq:
+            url_entry['changefreq'] = changefreq
+        if priority:
+            url_entry['priority'] = priority
+        self.sitemap_urls.append(url_entry)
+
+    def generate_sitemap_xml(self):
+        from xml.etree.ElementTree import Element, SubElement, tostring
+        sitemap = Element('urlset', xmlns='http://www.sitemaps.org/schemas/sitemap-image/1.1')
+        for url in self.sitemap_urls:
+            url_element = SubElement(sitemap, 'url')
+            for key, value in url.items():
+                SubElement(url_element, key).text = value
+        return tostring(sitemap, encoding='utf-8', method='xml').decode('utf-8')
